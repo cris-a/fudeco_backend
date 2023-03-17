d0d25ef815bc42aa681b9bc137d73ff37afb819a
cris-a <arcilacristian@gmail.com>
2023-03-17 16:42:43 -0300

    revisado las imagenes

--- src/controllers/uploaded.js
+++ src/controllers/uploaded.js
@@ -1,16 +1,28 @@
 import multer from 'multer';
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'uploads/');
-  },
-  filename: (req, file, cb) => {
-    cb(null, file.originalname);
-  },
-});
+// const storage = multer.diskStorage({
+//   destination: '../../../frontend/public/files',
+//   filename: (req, file, cb) => {
+//     let extension = file.originalname.slice(file.originalname.lastIndexOf('.'));
+//     cb(null, Date.now() + extension);
+//   },
+// });
 
-const upload = multer({
-  storage,
-}).single('file'); // 5mb
+// const upload = multer({
+//   storage,
+// }).single('file');
 
-export default upload;
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, 'uploads/');
+//   },
+//   filename: (req, file, cb) => {
+//     cb(null, file.originalname);
+//   },
+// });
+
+// const upload = multer({
+//   storage,
+// }).single('file'); // 5mb
+
+// export default upload;
